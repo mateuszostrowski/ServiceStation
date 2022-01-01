d7d16fcfe43f87ef2dad218f78a80a6f1fe1371d
mateuszostrowski <mateusz.ostrowski.lbn@gmail.com>
2022-01-01 21:46:30 +0100

    initial commit

---
+++ src/main/java/pl/infoshare/ServiceStationWeb/ServiceStationWebApplication.java
@@ -0,0 +1,13 @@
+package pl.infoshare.ServiceStationWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceStationWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceStationWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/infoshare/ServiceStationWeb/ServiceStationWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.infoshare.ServiceStationWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceStationWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
