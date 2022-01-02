cc4f49dc9ccf3b583ca30c3a489d2867bce3e2d8
mateuszostrowski <mateusz.ostrowski.lbn@gmail.com>
2022-01-02 14:40:46 +0100

    add form

--- src/main/java/pl/infoshare/controller/MainController.java
+++ src/main/java/pl/infoshare/controller/MainController.java
@@ -3,6 +3,7 @@ package pl.infoshare.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.infoshare.model.Car;
 import pl.infoshare.service.MenuService;
 
 @Controller
@@ -16,6 +17,7 @@ public class MainController {
 
     @GetMapping
     public String get(Model model){
+        model.addAttribute("car", new Car());
         model.addAttribute("menuObjects", menuService.get());
         return "index";
     }
--- src/main/java/pl/infoshare/controller/MenuController.java
+++ src/main/java/pl/infoshare/controller/MenuController.java
@@ -3,6 +3,7 @@ package pl.infoshare.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.infoshare.model.Car;
 import pl.infoshare.service.MenuService;
 
 @Controller
